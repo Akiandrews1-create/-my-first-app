89e7fbddb0c307b2b62ccd377c4030ff6d9ed2d1
Isaiah Andrews <isaiahandrews@Isaiahs-MacBook-Air.local>
2026-04-03 15:23:16 +0800

    fix deployment config

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 
@@ -35,5 +36,6 @@ def calculator():
 
     return render_template("index.html", result=result, history=history)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
